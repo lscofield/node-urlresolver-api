af710ba86b54530c3afc484af97ff4bcedb5c9f9
lscofield <lucasivan6801@hotmail.com>
2019-11-07 01:55:01 +0100

    Fix ok.ru resolver

--- servers/ok.js
+++ servers/ok.js
@@ -16,20 +16,21 @@ exports.index = function (req, res) {
         var json = $('#VideoAutoplayPlayerC').parent().attr('data-options');
         json = JSON.parse(json).flashvars.metadata;
         json = JSON.parse(json).videos;
+    
         for(var i = 0; i < json.length; i++){
-            if(json[i].name == 'hd'){
+            if(json[i].name == 'sd'){
                 mp4 = json[i].url;
                 break;
             }
         }
         if(mp4 == null && json.length > 0)
-            mp4 = json[0].url;
+            mp4 = json[json.length-1].url;
         
     } catch (e) {
         mp4 = null;
     }
 
-    mp4 = mp4 == null ? '' : mp4 + '/video/';
+    mp4 = mp4 == null ? '' : mp4;
 
     res.json({ status: mp4 == '' ? 'error' : 'ok', url: mp4 });
 };
