e78db5343ddd3b5c50aeee1d436a8609df4666f0
lscofield <lucasivan6801@hotmail.com>
2019-11-07 13:21:26 +0100

    New server: archive

--- servers/archive.js
+++ servers/archive.js
@@ -5,13 +5,19 @@
 
 exports.index = function (req, res) {
     const source = 'source' in req.body ? req.body.source : req.query.source;
+    const url = 'url' in req.body ? req.body.url : req.query.url;
+    // Cuando deje de funcionar el método de la url
+    // esta variable contendrá el codigo
+    // fuente de la página y se podrá hacer
+    // la extracción por codigo fuente
     const html = Buffer.from(source, 'base64').toString('utf8');
+    const link = Buffer.from(url, 'base64').toString('utf8');
     var mp4 = null;
 
-    if (html.includes('/download/'))
-        mp4 = html;
+    if (link.includes('/download/'))
+        mp4 = link;
     else {
-        const id = html.split("/")[4];
+        const id = link.split("/")[4];
         mp4 = `https://archive.org/download/${id}/${id}.mp4`;
     }
 
