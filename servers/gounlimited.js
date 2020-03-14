541b9ce75ac66a805104f30e1a58a762559713c9
lscofield <lucasivan6801@hotmail.com>
2020-03-14 18:28:45 +0100

    Fixed server: gounlimited

--- servers/gounlimited.js
+++ servers/gounlimited.js
@@ -3,7 +3,6 @@
  * GNU
  */
 
-const got = require('got');
 const json5 = require('json5');
 const unpacker = require('../lib/unpacker');
 const skkchecker = require('../lib/skkchecker');
@@ -25,37 +24,21 @@ exports.index = function (req, res) {
         res.json({ status: 'ok', url: granted });
     } else {
         // autorized app block
-        var url = Buffer.from(req.body.source, 'base64').toString('utf8');
+        var html = Buffer.from(req.body.source, 'base64').toString('utf8');
         var mp4 = '';
 
-        async function scrape(embedURL) {
-            var parsed = null;
-            try {
-                const { body } = await got(embedURL, {
-                    rejectUnauthorized: false
-                });
+        const packed = packedRegex.exec(html)[1];
+        const unpacked = unpacker.unPack(packed);
 
-                const packed = packedRegex.exec(body)[1];
-                const unpacked = unpacker.unPack(packed);
+        const sources = jsonRegex.exec(unpacked);
+        const stream = json5.parse(sources[1]);
 
-                const sources = jsonRegex.exec(unpacked);
-                parsed = json5.parse(sources[1]);
-            } catch (err) { }
+        try {
+            if (stream)
+                mp4 = stream[0];
+        } catch (err) { }
 
-            return parsed;
-        }
-
-
-        (async () => {
-            const stream = await scrape(url);
-
-            try {
-                if (stream)
-                    mp4 = stream[0];
-            } catch (err) { }
-
-            mp4 = mp4 == null ? '' : mp4;
-            res.json({ status: mp4 == '' ? 'error' : 'ok', url: mp4 });
-        })();
+        mp4 = mp4 == null ? '' : mp4;
+        res.json({ status: mp4 == '' ? 'error' : 'ok', url: mp4 });
     }
 };
